669681e366ef8596828b04437999983dfd793507
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2017-03-23 20:05:01 +0900

    Modify logger and channel_list

--- logger/src/epics_logger.cc
+++ logger/src/epics_logger.cc
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <fstream>
-#include <algorithm>
 #include <vector>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,86 +17,94 @@
 #define LOGGING_INTERVAL            30   //second
 #define NEW_FILE_INTERVAL        14400   //second
 
-std::vector<std::string> ChannelList;   //epics channel name
-std::vector<std::string> BranchList;    //tree branch name
-std::vector<double> BranchData;         //tree branch data
+namespace
+{
+  std::vector<std::string> ChannelList;   //epics channel name
+  std::vector<std::string> BranchList;    //tree branch name
+  std::vector<double> BranchData;         //tree branch data
 
-TTree *tree=0;
-time_t now;
-time_t last;
-std::string output_dir;
+  TTree *tree=0;
+  time_t now;
+  time_t last;
+  std::string output_dir;
 
-volatile int stop_flag=0; 
-void sigint_handler(int sig);
+  volatile int stop_flag=0;
+  void sigint_handler( int sig );
+  void GetEpicsData( void );
+  void PrintData( void );
+  void PrintTime( void );
+  void WriteRootFile( void );
+}
 
-void GetEpicsData();
-void PrintData();
-void PrintTime();
-void WriteRootFile();
-
-int main(int argc, char* argv[])
+//______________________________________________________________________________
+int
+main( int argc, char* argv[] )
 {
   if(argc !=3){
-    printf("Usage: %s <channel_list_file> <storage_path>\n",argv[0]);
-    return 0;
+    std::cout << "Usage: " << basename(argv[0])
+	      << " [channel_list_file] [storage_path]" << std::endl;
+    return EXIT_SUCCESS;
   }
-  
+
   // output directory check
   struct stat st;
   if( stat(argv[2], &st) ){
-    printf("cannot access %s: No such directory\n",argv[2]);
-    return 0;
+    std::cerr << "#E no such directory : " << argv[2] << std::endl;
+    return EXIT_FAILURE;
   }
   output_dir = argv[2];
 
   // channel_list_file check
   std::ifstream ifs(argv[1]);
-  if(!ifs){
-    printf("cannot access %s: No such file\n",argv[1]);
-    return 0;
+  if( !ifs.good() ){
+    std::cerr << "#E no such file : " << argv[1] << std::endl;
+    return EXIT_FAILURE;
   }
-  
+
   // read channel list
   std::string line;
-  while(ifs && getline(ifs,line)){
-    size_t pos;
+  while( ifs.good() && std::getline(ifs,line) ){
+    std::size_t pos;
     while( (pos=line.find_first_of(" ")) != std::string::npos ){
       line.erase(pos, 1);
     }
-    if(line[0]=='#' || line.empty() ) continue;
+    if( line.empty() || line[0]=='#' ) continue;
 
     // epics channel name
     ChannelList.push_back(line);
-    
+
     // tree branch name
     std::replace(line.begin(), line.end(), ':', '_');
     BranchList.push_back(line);
     BranchData.push_back(0);
-    
+
   }
   ifs.close();
 
   // print channel list
-  printf("====== EPICS channels =======\n");
-  for(int i=0; i<(int)ChannelList.size(); ++i){
-    printf("%d %s\n", i+1, ChannelList[i].c_str() );
+  std::cout << "====== EPICS channels =======" << std::endl;
+  for( std::size_t i=0; i<ChannelList.size(); ++i ){
+    std::cout << std::setw(3)  << i+1 << " "
+	      << ChannelList[i] << std::endl;
   }
-  printf("=============================\n");
+  std::cout << "=============================" << std::endl;
 
   // create tree
   tree = new TTree("tree", "K1.8 EPICS Data");
   tree->Branch("UnixTime", &now, "UnixTime/I");
-  for(int i=0; i<(int)BranchList.size(); ++i){
-    tree->Branch(BranchList[i].c_str(), &BranchData[i], Form("%s/D",BranchList[i].c_str()));
+  for( std::size_t i=0; i<BranchList.size(); ++i ){
+    tree->Branch( BranchList[i].c_str(),
+		  &BranchData[i],
+		  Form("%s/D",BranchList[i].c_str()) );
   }
-  
-  signal(SIGINT, sigint_handler);
+
+  ::signal(SIGINT, sigint_handler);
 
   last = time(0);
-  
+
   while(stop_flag==0){
     now = time(0);
-    
+
     GetEpicsData();
     //PrintData();
     PrintTime();
@@ -108,28 +118,33 @@ int main(int argc, char* argv[])
     int rest = LOGGING_INTERVAL - (time(0) - now);
     if( rest>0 ) sleep(rest);
   }
-  
+
   WriteRootFile();
 
   return 0;
 }
 
-void WriteRootFile()
+namespace
+{
+
+void WriteRootFile( void )
 {
   char s[256];
   struct tm *p = localtime(&last);
   strftime(s, 256, "%Y%m%d_%H%M%S", p);
 
-  std::string filename = output_dir + "/" + "k18epics_" + s + ".root"; 
+  std::string filename = output_dir + "/" + "k18epics_" + s + ".root";
   TFile* file = new TFile(filename.c_str(), "RECREATE");
   tree->Write();
   file->Close();
   tree->Reset();
-  
-  printf("\n\nCreate ROOT File: %s\n\n\n",filename.c_str());
+
+  std::cout << std::endl << std::endl
+	    << "Create ROOT File : " << filename
+	    << std::endl << std::endl;
 }
 
-void GetEpicsData()
+void GetEpicsData( void )
 {
   //std::string caget = "caget -w 0.1 -t ";
   std::string caget = "caget -w 30 -t ";
@@ -137,13 +152,13 @@ void GetEpicsData()
   FILE* fp;
   char input[128];
 
-  for(int i=0; i<(int)ChannelList.size(); ++i){
+  for( std::size_t i=0; i<ChannelList.size(); ++i ){
     BranchData[i] = TREE_DEFAULT_VALUE;
     cmdline = caget+ChannelList[i];
     fp=popen(cmdline.c_str(),"r");
     fgets(input, 128, fp);
     if(pclose(fp) !=0) continue;
-    
+
     double data1,data2,data3;
     int ret = sscanf(input,"%lf %lf %lf",&data1, &data2, &data3);
     if(ret==1){
@@ -155,14 +170,14 @@ void GetEpicsData()
   }
 }
 
-void PrintData()
+void PrintData( void )
 {
   char s[256];
   struct tm *p = localtime(&now);
   strftime(s, 256, "%Y/%m/%d %H:%M:%S", p);
-  printf("\nTime: %s\n",s);
-  for(int i=0; i<(int)ChannelList.size(); ++i){
-    printf("%s = %f\n",ChannelList[i].c_str(), BranchData[i]);
+  std::cout << std::endl << "Time: " << s << std::endl;
+  for( std::size_t i=0; i<ChannelList.size(); ++i ){
+    std::cout << ChannelList[i] << " = " << BranchData[i] << std::endl;
   }
 }
 
@@ -172,12 +187,17 @@ void PrintTime()
   struct tm *p = localtime(&now);
   strftime(s, 256, "%Y/%m/%d %H:%M:%S", p);
   int remain = NEW_FILE_INTERVAL-(now-last);
-  printf("Last Log Time: %s  [%d]\n",s, remain);
+  std::cout << "Last Log Time : " << s
+	    << " [" << remain << "]" << std::endl;
 }
 
-void sigint_handler(int sig)
+void sigint_handler( int sig )
 {
   signal( SIGINT, SIG_IGN );
-  printf("\n\nReceived SIGINT\n\n");
+  std::cout << std::endl << std::endl
+	    << "Received SIGINT"
+	    << std::endl << std::endl;
   stop_flag = -1;
+}
+
 }
