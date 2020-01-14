ab0da89cdfbb14e0b5c37a5a36a0e6ec793f4911
S.H. Hayakawa <hayashu@post.j-parc.jp>
2020-01-14 14:20:01 +0900

    Add air pressure as GAS:TPC:AIRP

--- epicsApp/iocBoot/iocK18Monitor/gas.cmd
+++ epicsApp/iocBoot/iocK18Monitor/gas.cmd
@@ -16,6 +16,7 @@ K18Monitor_registerRecordDeviceDriver pdbbase
 dbLoadRecords("db/GAS.db", "det_name=SDC2,data_type=DIFP")
 dbLoadRecords("db/GAS.db", "det_name=SDC3,data_type=DIFP")
 dbLoadRecords("db/GAS.db", "det_name=TPC,data_type=DIFP")
+dbLoadRecords("db/GAS.db", "det_name=TPC,data_type=AIRP")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
