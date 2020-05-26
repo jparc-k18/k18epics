94ace77c8809e669ec4fc3f60813b5e2e1a8065a
S.H. Hayakawa <hayashu@post.j-parc.jp>
2020-05-27 05:24:37 +0900

    Modify MppcBias using caput

--- epicsApp/K18MonitorApp/src/devMppcBias.cc
+++ epicsApp/K18MonitorApp/src/devMppcBias.cc
@@ -21,81 +21,15 @@
 #include "recSup.h"
 #include "devSup.h"
 #include "link.h"
-#include "waveformRecord.h"
+#include "aiRecord.h"
 #include "epicsExport.h"
 
-static long read_wf(waveformRecord *rec)
+static long read_ai(aiRecord *rec)
 {
-  float* ptr = (float*)rec->bptr;
-
-  static std::string summary_txt("/data3/E40SubData2019Feb/epics_2019feb/mppcbias_summary.txt");
-  // static std::string summary_txt("/data3/E40SubData/epics_2018jun/mppcbias_summary.txt");
-  std::ifstream ifs( summary_txt.c_str() );
-  if( !ifs.is_open() ){
-    std::cerr << "ERROR: cannot open file: " << summary_txt << std::endl;
-    rec->nord = 0;
-    return 0;
-  }
-
-  // std::cout << std::time(0) << std::endl;
-
-  std::string line;
-  int ndata = 0;
-  while( !ifs.eof() && std::getline(ifs, line) ){
-    // std::cout << line << std::endl;
-    std::stringstream ss(line);
-    std::string label;
-    std::string state;
-    ss >> label >> state;
-    if( label.find("192.168.") == 0 ){
-      if( state.find("OFF") == 0 )
-	ptr[ndata++] = 0.;
-      else if( state.find("ON") == 0 )
-	ptr[ndata++] = 100.;
-      else
-	ptr[ndata++] = -100.;
-    }
-    if( label.find("CH") == 0 ){
-      std::string date;
-      std::string time;
-      double vset = -9999.;
-      double vmon = -9999.;
-      double imon = -9999.;
-      double temp = -9999.;
-      ss >> date >> time >> vset >> vmon >> imon >> temp;
-      if( state.find("OFF") == 0 )
-	ptr[ndata++] = 0.;
-      else if( state.find("ON") == 0 )
-	ptr[ndata++] = 100.;
-      else if( state.find("NOUPDATE") == 0 )
-	ptr[ndata++] = 200.;
-      else
-	ptr[ndata++] = -100.;
-      ptr[ndata++] = vset * 100.;
-      ptr[ndata++] = vmon * 100.;
-      ptr[ndata++] = imon * 100.;
-      ptr[ndata++] = temp * 100.;
-    }
-  }
-
-#if 0
-  int i = 0;
-  for( int b=0; b<2; ++b ){
-    std::cout << i << " " << ptr[i++] << std::endl;
-    for( int j=0; j<8; ++j ){
-      for( int j=0; j<5; ++j ){
-	std::cout << ptr[i++] << " ";
-      }
-      std::cout << std::endl;
-    }
-  }
-#endif
-
-  rec->nord = ndata;
-  return 0;
+  return 2;
 }
 
-static long init_record(waveformRecord *rec, int pass)
+static long init_record(aiRecord *rec)
 {
   return 0;
 }
@@ -106,7 +40,7 @@ struct IOC{
   DEVSUPFUN init;
   DEVSUPFUN init_record;
   DEVSUPFUN get_ioint_info;
-  DEVSUPFUN read_wf;
+  DEVSUPFUN read_ai;
   DEVSUPFUN special_linconv;
 };
 
@@ -116,7 +50,7 @@ struct IOC devMppcBias={
   NULL,
   (DEVSUPFUN) init_record,
   NULL,
-  (DEVSUPFUN) read_wf,
+  (DEVSUPFUN) read_ai,
   NULL
 };
 epicsExportAddress(dset,devMppcBias);
