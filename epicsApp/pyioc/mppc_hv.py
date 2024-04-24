cc9a83f90e5debd1c8dcd8f190128795f25dcfb2
Rintaro Kurata <kurata.rintaro.s7@dc.tohoku.ac.jp>
2024-04-25 06:25:59 +0900

    Update pyioc of mppc_hv for kikusui & hamamatsu

--- epicsApp/pyioc/mppc_hv.py
+++ epicsApp/pyioc/mppc_hv.py
@@ -5,26 +5,24 @@ import epics
 import time
 
 mppchv_text = '/misc/subdata/MPPC_HV/mppcbias_summary.txt'
-interval = 5
+error_log="error_log/error_mppc_hv.txt"
+interval = 1
 
 if __name__ == '__main__':
-#  try:
-    print(datetime.datetime.now(), 'IOC start')
-    while True:
-      start = time.time()
-      count = 0
-      linenumber = 0
+  print(datetime.datetime.now(), 'IOC start')
+  while True:
+    print(datetime.datetime.now())
+    start = time.time()
+    count = 0
+    linenumber = 0
+    try:
       with open(mppchv_text, 'r') as f:
         lines = f.readlines()
-        print('open file')
         for line in lines:
           columns = line.split()
           if len(columns) == 2:
             if columns[0] == "192.168.30.81":
               ch_header = 'MPPC:BFT:'
-              print("ch_header"+str(ch_header))
-#            elif columns[0] == "192.168.30.80":
- #             ch_header = 'MPPC:AFT:'
               if columns[1] =="ON":
                 state = 1
               elif columns[1] == "OFF":
@@ -32,7 +30,6 @@ if __name__ == '__main__':
               else:
                 state = -1
               epics.caput(ch_header+"BOARD", state, wait=1.0)
-              print("state"+str(state))
           if len(columns) == 8:
             if columns[1] =="ON":
               state = 1
@@ -41,13 +38,9 @@ if __name__ == '__main__':
             else:
               state = -1
             hv_set = columns[4]
-            print("hv_set"+str(hv_set))
             hv_mon = columns[5]
-            print("hv_mon"+str(hv_mon))
             i_mon = columns[6]
-            print("i_mon"+str(i_mon))
             temp = columns[7]
-            print("temp"+str(temp))
             ch_name = ch_header + columns[0]
             epics.caput(ch_name+':STAT', state, wait=1.0)
             epics.caput(ch_name+':VSET', hv_set, wait=1.0)
@@ -58,7 +51,40 @@ if __name__ == '__main__':
             print(ch_name+":VSET", hv_set)
             print(ch_name+":VMON", hv_mon)
             print(ch_name+":IMON", i_mon)
-            print(ch_name+":TEMP", temp)
-        time.sleep(interval - (time.time() - start))
- # except:
-  #  pass
+            print(ch_name+":TEMP", temp, "\n")
+    except FileNotFoundError as e:
+      error_time = datetime.datetime.now()
+      print("\033[31m", error_time)
+      print("cannot open the file: mppc_bias_summary.txt\033[0m")
+      with open(error_log, 'a') as errf:
+        errf.write(str(error_time) + "\n")
+        errf.write("Error Type: File not found\n")
+        errf.write(f"Detail: {str(e)}\n")
+        errf.write("-"*25 + "\n")
+      continue
+    except PermissionError as e:
+      error_time = datetime.datetime.now()
+      print("\033[31m", error_time)
+      print("permision denied: mppc_bias_summary.txt\033[0m")
+      with open(error_log, 'a') as errf:
+        errf.write(str(error_time) + "\n")
+        errf.write("Error Type: PernissionError\n")
+        errf.write(f"Detail: {str(e)}\n")
+        errf.write("-"*25 + "\n")
+      continue
+    except OSError as e:
+      error_time = datetime.datetime.now()
+      print("\033[31m", error_time)
+      print("OS error occured: mppc_bias_summary.txt")
+      print("Error code: ", e.errno)
+      print("Error msg: ", e.strerror, "\033[0m")
+      with open(error_log, 'a') as errf:
+        errf.write(str(error_time) + "\n")
+        errf.write("Error Type: OSError\n")
+        errf.write(f"Detail: {str(e)}\n")
+        errf.write("-"*25 + "\n")
+      continue
+    if interval - (time.time() - start) < 0:
+      continue
+    print("-"*35)
+    time.sleep(interval - (time.time() - start))
--- epicsApp/pyioc/mppc_kikusui_hv.py
+++ epicsApp/pyioc/mppc_kikusui_hv.py
@@ -4,13 +4,14 @@ import datetime
 import epics
 import time
 
-
-interval = 5
+interval = 1
 N=0
+error_log="error_log/error_mppc_kikusui_hv.txt"
+
 if __name__ == '__main__':
-#  try:
   print(datetime.datetime.now(), 'IOC start')
   while True:
+    print(datetime.datetime.now())
     for i in range(1,5):
       start = time.time()
       count = 0
@@ -18,37 +19,62 @@ if __name__ == '__main__':
       mppchv_text = ""
       mppchv_text = f"/misc/subdata/AFT_MPPCHV/MPPC_HV{i}/mppcbias_summary.txt"
       ch_header = f"MPPC:AFT:CH{i}"
-      print(mppchv_text) # debug
-      with open(mppchv_text, 'r') as f:
-        lines = f.readlines()
-        print('open file')
-        for line in lines:
-          print(line) # debug
-          columns = line.split()
-          print(columns[0])
-          if columns[0] =="ON":
-            state = 1
-          elif columns[0] == "OFF":
-            state = 0
-          else:
-            state = -1
-          print(columns[0])
-          hv_set = columns[1]
-          print(columns[1])
-          hv_mon = columns[2]
-          i_mon = columns[3]
-          for j in range(0,4):
-            print(columns[j])
-          epics.caput(ch_header+':STAT', state, wait=1.0)
-          epics.caput(ch_header+':VSET', hv_set, wait=1.0)
-          epics.caput(ch_header+':VMON', hv_mon, wait=1.0)
-          epics.caput(ch_header+':IMON', i_mon, wait=1.0)
-          print(ch_header+":STAT", state)
-          print(ch_header+":VSET", hv_set)
-          print(ch_header+":VMON", hv_mon)
-          print(ch_header+":IMON", i_mon)
-          N+=1
-          print(N)
+      # print(mppchv_text) # debug
+      try:
+        with open(mppchv_text, 'r') as f:
+          lines = f.readlines()
+          for line in lines:
+            columns = line.split()
+            if columns[0] =="ON":
+              state = 1
+            elif columns[0] == "OFF":
+              state = 0
+            else:
+              state = -1
+            hv_set = columns[1]
+            hv_mon = columns[2]
+            i_mon = columns[3]
+            epics.caput(ch_header+':STAT', state, wait=1.0)
+            epics.caput(ch_header+':VSET', hv_set, wait=1.0)
+            epics.caput(ch_header+':VMON', hv_mon, wait=1.0)
+            epics.caput(ch_header+':IMON', i_mon, wait=1.0)
+            print(ch_header+":STAT", state)
+            print(ch_header+":VSET", hv_set)
+            print(ch_header+":VMON", hv_mon)
+            print(ch_header+":IMON", i_mon, "\n")
+      except FileNotFoundError as e:
+        error_time = datetime.datetime.now()
+        print("\033[31m", error_time)
+        print("cannot open the file: MPPC_HV" + str(i) + "/mppc_bias_summary.txt\033[0m")
+        with open(error_log, 'a') as errf:
+          errf.write(str(error_time) + "\n")
+          errf.write("Error Type: File not found\n")
+          errf.write(f"Detail: {str(e)}\n")
+          errf.write("-"*25 + "\n")
+        continue
+      except PermissionError as e:
+        error_time = datetime.datetime.now()
+        print("\033[31m", error_time)
+        print("permision denied: MPPC_HV" + str(i) + "/mppc_bias_summary.txt\033[0m")
+        with open(error_log, 'a') as errf:
+          errf.write(str(error_time) + "\n")
+          errf.write("Error Type: PernissionError\n")
+          errf.write(f"Detail: {str(e)}\n")
+          errf.write("-"*25 + "\n")
+        continue
+      except OSError as e:
+        error_time = datetime.datetime.now()
+        print("\033[31m", error_time)
+        print("OS error occured: MPPC_HV" + str(i) + "/mppc_bias_summary.txt")
+        print("Error code: ", e.errno)
+        print("Error msg: ", e.strerror, "\033[0m")
+        with open(error_log, 'a') as errf:
+          errf.write(str(error_time) + "\n")
+          errf.write("Error Type: OSError\n")
+          errf.write(f"Detail: {str(e)}\n")
+          errf.write("-"*25 + "\n")
+        continue
+    if interval - (time.time() - start) < 0:
+      continue
+    print("-"*35)
     time.sleep(interval - (time.time() - start))
- # except:
-  #  pass
